5616f591bd04077153c1da14966c27462722910b
mxlxm24 <mx.lxm32@gmail.com>
2023-11-03 18:27:46 +0300

    Model ve Migrationlar oluşturuldu

---
+++ database/migrations/2023_11_03_145539_create_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedSmallInteger('user_id');
+            $table->string('vize1');
+            $table->string('vize2');
+            $table->string('final');
+
+            $table->foreign('user_id')->on('user')->references('id');
+
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
